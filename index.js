b6615734c39fc79d51abeae98fc6e338e77cbb63
tsunpot <47371034+tsunpot@users.noreply.github.com>
2019-10-31 12:25:39 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+// Version 1.30 r:00
+
+const config = require('./config.json');
+
+module.exports = function AutoCutscene(m) {
+	const cmd = m.command || m.require.command
+
+	// config
+	let enable = config.enable;
+
+	// command
+	// toggle
+	cmd.add('skip', {
+		$none() {
+			enable = !enable;
+			send(`${enable ? 'Enabled' : 'Disabled'}`);
+		}
+	});
+
+	// code
+	m.hook('S_PLAY_MOVIE', 1, (e) => {
+		if (!enable) return
+		m.send('C_END_MOVIE', 1, Object.assign({ unk : 1 }, e));
+		return false
+	});
+
+	// helper
+	function send(msg) { cmd.message(`: ` + msg); }
+
+}
